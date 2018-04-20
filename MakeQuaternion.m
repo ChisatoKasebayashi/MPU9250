d63f2319c5e3377bb65b4c03597cadfe461890b6
tilly1113 <chisato.rukia@gmail.com>
2018-04-20 14:35:48 +0900

    update

--- MakeQuaternion.m
+++ MakeQuaternion.m
@@ -1,4 +1,8 @@
 function res = MakeQuaternion(u) %size(u):3x1
 u_norm = norm(u);
-res = [cos(u_norm/2); (u(1)/u_norm)*sin(u_norm/2); (u(2)/u_norm)*sin(u_norm/2); (u(3)/u_norm)*sin(u_norm/2) ];
+if u_norm == 0
+    res = [1;0;0;0];
+else
+    res = [cos(u_norm/2); (u(1)/u_norm)*sin(u_norm/2); (u(2)/u_norm)*sin(u_norm/2); (u(3)/u_norm)*sin(u_norm/2) ];
+end
 end
--- QuaternionMultiply.m
+++ QuaternionMultiply.m
@@ -1,7 +1,7 @@
-function QuaternionMultiply(q, p) % size(q):1x4 size(p):1x4
+function res = QuaternionMultiply(q, p) % size(q):1x4 size(p):1x4
 C = [0, -q(4), q(3);
     q(4), 0, -q(2);
     -q(3), q(2), 0];
-ans = [p(1) -transpose(p(2:4,1));
+res = [p(1) -transpose(p(2:4,1)); 
        p(2:4,1) p(1)*eye(3)+C ] * [q(1) q(2:4,1)]';
 end
