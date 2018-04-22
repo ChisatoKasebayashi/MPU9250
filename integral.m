cfbf63221134076e771533797a6b8350d23a218b
tilly1113 <chisato.rukia@gmail.com>
2018-04-22 17:20:37 +0900

    test

--- integral.m
+++ integral.m
@@ -4,20 +4,29 @@ Hz = 80;
 delta_t = 1/Hz;
 u = [0,0,0];
 deg = [];
+qX = [];
+qY = [];
+qZ = [];
 
 f_skip = 10;
 for i=1:length(data)
     %disp(MakeQuaternion(u));
     q = QuaternionMultiply(MakeQuaternion(u), MakeQuaternion(gyro(i,:) * delta_t));
-    if rem(i,f_skip) == 0
+    %if rem(i,f_skip) == 0
         norm = sqrt(sum(q(2:4).^2));
         q_normal = q / norm;
         q_normal(1) = q(1);
-        qX = QuaternionMultiply(QuaternionMultiply(q_normal, MakeQuaternion(u)), QuaternionInverse(q_normal) );
-        qY = QuaternionMultiply(QuaternionMultiply(q_normal, MakeQuaternion(u)), QuaternionInverse(q_normal) );
-        qZ = QuaternionMultiply(QuaternionMultiply(q_normal, MakeQuaternion(u)), QuaternionInverse(q_normal) );
+        if i == 1
+            qX = QuaternionMultiply(QuaternionMultiply(q_normal, [0;1;0;0]), QuaternionInverse(q_normal) );
+            qY = QuaternionMultiply(QuaternionMultiply(q_normal, [0;0;1;0]), QuaternionInverse(q_normal) );
+            qZ = QuaternionMultiply(QuaternionMultiply(q_normal, [0;0;0;1]), QuaternionInverse(q_normal) );
+        else
+            qX = QuaternionMultiply(QuaternionMultiply(q_normal, qX), QuaternionInverse(q_normal) );
+            qY = QuaternionMultiply(QuaternionMultiply(q_normal, qY), QuaternionInverse(q_normal) );
+            qZ = QuaternionMultiply(QuaternionMultiply(q_normal, qZ), QuaternionInverse(q_normal) );
+        end
         plotQuaternion(qX,qY,qZ); 
-    end
+    %end
     u = Quaternion2RotationVector(q);
     deg = [deg; u];
 end
